aa4808cee81610e0aa12a9c1bf1de12ef6e9dde3
arimizuyuuta <k024g1028@g.neec.ac.jp>
2025-05-23 14:30:27 +0900

    AAA

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "IShape.h"
+#include "Circle.h"
+#include "Rectangle.h"
 
-int main() {
-	printf("あいうえお");
+int main(void) {
 
-		return 0;
+	IShape* shape[2];
+
+	shape[0] = new Circle();
+	shape[1] = new Rectangle();
+
+	printf("円の半径:4\n短形の底辺:2\n短形の高さ:3\n");
+
+	for (int i = 0; i < 2; i++)
+		shape[i]->Size();
+
+	printf("\n");
+
+	for (int i = 0; i < 2; i++)
+		shape[i]->Draw();
+
+	// メモリ解放
+	for (int i = 0; i < 2; i++)
+		delete shape[i];
+
+	return 0;
 }
