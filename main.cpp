aa8a98f167b8214b5ec0e6c06446b7308b7b14a5
arimizuyuuta <k024g1028@g.neec.ac.jp>
2025-05-23 14:11:35 +0900

    AAA

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include<stdio.h>
+#include <cstdio>
+using namespace std;
+
+// 動物クラス（抽象クラス）
+class Animal {
+public:
+    virtual void sound() const = 0; // 純粋仮想関数
+};
+
+// 犬クラス（Animal を継承）
+class Dog : public Animal {
+public:
+    void sound() const override {
+        printf("イヌ鳴き声：ワン\n");
+    }
+};
+
+// 猫クラス（Animal を継承）
+class Cat : public Animal {
+public:
+    void sound() const override {
+        printf("ネコ鳴き声：ニャー\n");
+    }
+};
 
 int main() {
-	printf("あいうえお");
+    Animal* animal1 = new Dog(); // 犬のインスタンス
+    Animal* animal2 = new Cat(); // 猫のインスタンス
 
-		return 0;
+    animal1->sound(); // ポリモーフィズムにより Dog の sound() が呼ばれる
+    animal2->sound(); // 同上、Cat の sound()
+
+    delete animal1;
+    delete animal2;
+
+    return 0;
 }
